fef772f71709c83208eab8db42dc784a2037bbe7
shrutik29 <159531925+shrutik29@users.noreply.github.com>
2024-02-18 03:06:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, {useState, useEffect} from 'react';
+import { Navbar, Container } from "react-bootstrap";
+
+
+
+import {Cards} from './Components/Cards'
+import {Filter} from './Components/Filter'
+import useWindowSize from './Hooks/useWindowSize';
+
 function App() {
+
+  const [error,setError] = useState(false)
+  const [category, setCategory] = useState([]);
+
+  const size = useWindowSize();  
+   
+  
+
   return (
+        // Creating Navbar
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+           <Container className='m-0'>
+              <Navbar.Brand href="#" className='fs-3 fw-3'>Headlines</Navbar.Brand>
+           </Container>
+         </Navbar>
+    <div style={{
+      display: 'flex',
+      }}>
+        
+      {/* Filter Section */}
+
+
+      {size.width > 600 && <div style={{
+            width: '27vw',
+            marginLeft:'1vw'
+        }}>
+            <Filter setCategory={setCategory}/>
+            
+       </div>}
+
+
+        {/* Cards Section */}
+
+        <div className='newsfeed'>
+            
+          <div className='cards'><Cards category={category} setError={setError}/></div>
+       
+        </div>
+    
     </div>
+
+ </div>
   );
 }
 
